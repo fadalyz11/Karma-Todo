cf5dc75cb593f3124ed434986554bc306f113c93
fadalyz11 <fadalyz11@gmail.com>
2019-08-15 02:15:14 +0200

    The Caching

--- public/browser.js
+++ public/browser.js
@@ -1,18 +1,63 @@
+function itemTemplate() {
+  return `Hello The Function`;
+}
+
+let createField = document.getElementById("create-field");
+
+// Create Feature
+document.getElementById("create-form").addEventListener("submit", function(e) {
+  e.preventDefault();
+  axios
+    .post("/create-item", {
+      text: createField.value
+    })
+    .then(function() {
+      document
+        .getElementById("item-list")
+        .insertAdjacentHTML("beforeend", itemTemplate());
+    })
+    .catch(function() {
+      console.log("try again");
+    });
+});
+
 document.addEventListener("click", function(e) {
+  // Delete Feature
+  if (e.target.classList.contains("delete-me")) {
+    if (confirm("Do you really want to delete this item")) {
+      axios
+        .post("/delete-item", {
+          id: e.target.getAttribute("data-id")
+        })
+        .then(function() {
+          e.target.parentElement.parentElement.remove();
+        })
+        .catch(function() {
+          console.log("try again");
+        });
+    }
+  }
+
+  // Update Feature
   if (e.target.classList.contains("edit-me")) {
-    let userInput = prompt("Enter you desired new text");
-    axios
-      .post("/update-item", {
-        text: userInput,
-        id: e.target.getAttribute("data-id")
-      })
-      .then(function() {
-        e.target.parentElement.parentElement.querySelector(
-          ".item-text"
-        ).innerHTML = userInput;
-      })
-      .catch(function() {
-        console.log("try again");
-      });
+    let userInput = prompt(
+      "Enter you desired new text",
+      e.target.parentElement.parentElement.querySelector(".item-text").innerHTML
+    );
+    if (userInput) {
+      axios
+        .post("/update-item", {
+          text: userInput,
+          id: e.target.getAttribute("data-id")
+        })
+        .then(function() {
+          e.target.parentElement.parentElement.querySelector(
+            ".item-text"
+          ).innerHTML = userInput;
+        })
+        .catch(function() {
+          console.log("try again");
+        });
+    }
   }
 });
